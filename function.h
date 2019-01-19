9a0af0969a024d0c9a51f4d1c0e34145e6d71550
Motyaspr <sprikutm@gmail.com>
2019-01-19 19:32:35 +0300

    small object

--- function.h
+++ function.h
@@ -5,7 +5,8 @@
 #include <variant>
 #include <memory>
 
-constexpr const size_t MAX_SIZE = 16;
+static const size_t B_SIZE = 64;
+
 
 template <typename T>
 class function;
@@ -13,48 +14,102 @@ class function;
 template <typename Ret, typename ...Args>
 class function<Ret (Args...)> {
 public:
-    Ret operator()(Args...args) {
-        return holder->invoke(args...);
+    Ret operator()(Args...args) const {
+        if (!is_small)
+            return holder->invoke(args...);
+        function_holder_base * tmp= (function_holder_base *)buf;
+        return tmp->invoke(args...);
     }
 
-    function() noexcept : holder(nullptr) {}
+    function() noexcept : holder(nullptr), is_small(false) {}
 
-    function(std::nullptr_t) noexcept : holder(nullptr) {}
+    function(std::nullptr_t) noexcept : holder(nullptr), is_small(false) {}
 
-    function(const function &other) : holder(std::move(other.holder->copy())) {
+    function(const function &other) {
+        function_holder_base * tmp = (function_holder_base *)other.buf;
+        is_small = other.is_small;
+        if (other.is_small)
+            memcpy(buf, other.buf, B_SIZE);
+        else
+            holder = other.holder->copy();
+
     }
 
-    function(function &&other) noexcept : holder(std::move(other.holder)) {
+    function(function &&other) noexcept{
+        function_holder_base * tmp = (function_holder_base *)other.buf;
+        is_small = other.is_small;
+        if (other.is_small)
+            tmp->create_small_copy(buf);
+        else
+            new(buf) std::unique_ptr<function_holder_base>(std::move(other.holder));
     }
 
     function &operator=(const function &other) {
-        holder(std::move(other.holder->copy()));
+        function tmp(other);
+        swap(other);
         return *this;
     }
 
     function &operator=(function &&other) noexcept {
-        holder(std::move(other.holder));
+        function_holder_base * tmp = (function_holder_base *)other.buf;
+        if (is_small)
+            ((function_holder_base *) other.buf)->~function_holder_base();
+        else
+            holder.reset();
+
+        is_small = other.is_small;
+        if (other.is_small)
+            tmp->create_small_copy(buf);
+        else
+            new(buf) std::unique_ptr<function_holder_base>(std::move(other.holder));
         return *this;
     }
 
     void swap(function& other) noexcept {
-        std::swap(holder, other.holder);
+        std::swap(buf, other.buf);
+        std::swap(is_small, other.is_small);
     }
 
     explicit operator bool() const noexcept {
+        if (is_small)
+            return true;
         return static_cast<bool>(holder);
     }
 
     template<typename T>
-    function &operator=(const T &t) {
-        holder = std::make_unique<function_holder<T>>(t);
+    function &operator=(T t) {
+        if (sizeof(T) < B_SIZE){
+            is_small = true;
+            new(buf) function_holder<T>(t);
+        }
+        else{
+            is_small = false;
+            holder = std::make_unique<function_holder<T>>(std::move(t));
+        }
         return *this;
     }
 
     template<typename T>
     function(T t) {
-        holder = std::make_unique<function_holder<T>>(std::move(t));
+        if (sizeof(T) < B_SIZE){
+            is_small = true;
+            new(buf) function_holder<T>(t);
+        }
+        else{
+            is_small = false;
+            holder = std::make_unique<function_holder<T>>(std::move(t));
+        }
     }
+
+    ~function() {
+        if (is_small) {
+            function_holder_base *m = (function_holder_base *) buf;
+            m->~function_holder_base();
+        } else {
+            holder.reset();
+        }
+    }
+
 
 
     class function_holder_base {
@@ -62,6 +117,8 @@ public:
         function_holder_base() {};
         virtual ~function_holder_base() {};
         virtual Ret invoke(Args...) = 0;
+        virtual void create_small_copy(void *pos) = 0;
+
         virtual std::unique_ptr<function_holder_base> copy() const = 0;
     };
 
@@ -80,14 +137,24 @@ public:
             return std::make_unique<function_holder<Func>>(f);
         }
 
+        void create_small_copy(void *adr) override {
+            new(adr) function_holder<Func>(f);
+        }
+
+        /*void create_big_copy(void *adr) override {
+            new(adr) std::unique_ptr<function_holder<Func>>(new function_holder<Func>(f));
+        }*/
+
         Func f;
 
     };
 
 private:
-
+union {
     std::unique_ptr<function_holder_base> holder;
+    char buf[B_SIZE];
 };
-
+    bool is_small;
+};
 
 #endif /
--- main.cpp
+++ main.cpp
@@ -1,2 +1,125 @@
 #include <iostream>
-int main(){std::cout<<"Hello, World!"<<std::endl;return 0;}
+#include <cassert>
+#include "function.h"
+#include <functional>
+
+int foo() {
+    return 1;
+}
+
+int bar() {
+    return 2;
+}
+
+double pi() {
+    // kak grubo
+    return 3.14;
+}
+
+void test_defaultConstructor() {
+    function<int(void)> f(foo);
+    assert(f() == 1);
+}
+
+void test_copyConstructor() {
+    function<int(void)> b(bar);
+    function<int(void)> second(b);
+    assert(second() == 2);
+}
+
+void test_nullptrConstructor() {
+    function<void(void)> f(nullptr);
+}
+
+void test_moveConstructor() {
+    function<int(void)> b(bar);
+    function<int(void)> second(std::move(b));
+    assert(second() == 2);
+}
+
+void test_operatorAssignment() {
+    function<int(void)> b(bar);
+    function<int(void)> second = b;
+    assert(second() == 2);
+}
+
+void test_moveAssignment() {
+    function<int(void)> b(bar);
+    function<int(void)> second(foo);
+    second = std::move(b);
+    assert(second() == 2);
+}
+
+void test_explicitOperatorBool() {
+    function<int(void)> f(nullptr);
+    assert(!f);
+    f = foo;
+    assert(f);
+}
+
+void test_lambda() {
+    int a = 10;
+    function<int(int)> l = [a](int x) {
+        return a + x;
+    };
+    assert(l(5) == 15);
+}
+
+void test_swap() {
+    function<int()> f(foo);
+    function<int()> b(bar);
+    assert(f() == 1);
+    assert(b() == 2);
+
+    f.swap(b);
+
+    assert(f() == 2);
+    assert(b() == 1);
+}
+
+void test_diffTypes() {
+    function<int()> f = foo;
+    assert(f() == 1);
+    f = pi;
+    assert(pi() == 3.14);
+}
+
+
+void NIKITOZZZZ_test() {
+    int foo = 1;
+    double bar = 3;
+    double bar2 = 3;
+    double bar3 = 3;
+
+    function<int (std::ostream &)> f([=](std::ostream &os) mutable {
+        os << "test " << foo << " " << bar << std::endl;
+        os << "test " << bar2 << " " << bar3 << std::endl;
+        foo *= 2;
+        foo += 2;
+        bar -= 0.1;
+        os << "test " << foo << " " << bar << std::endl;
+        return foo;
+    });
+
+    f(std::cout);
+}
+
+
+void all_test() {
+    test_defaultConstructor();
+    test_copyConstructor();
+    test_nullptrConstructor();
+    test_moveConstructor();
+    test_operatorAssignment();
+    test_moveAssignment();
+    test_explicitOperatorBool();
+    test_swap();
+    test_lambda();
+    test_diffTypes();
+    NIKITOZZZZ_test();
+}
+
+int main() {
+    all_test();
+    return 0;
+}
